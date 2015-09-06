5445d8c98b245336420efe9866c91f7a9dbbd383
Arnab Chaudhuri <coolguyforgenerationx@gmail.com>
2015-09-06 23:06:09 +0530

    Revert "Fixed /system partition resize(Applicable only with TWRP-2.8.7.0 and above). Using 'resize2fs' from /sbin directory."
    This reverts commit b4a559e1fdb1f204182e143fcd39162a65afa72a.

--- releasetools/extensions/releasetools.py
+++ releasetools/extensions/releasetools.py
@@ -5,6 +5,6 @@ def FullOTA_PostValidate(info):
 	# run e2fsck
 	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/mmcblk0p9");');
 	# resize2fs: run and delete
-	info.script.AppendExtra('run_program("/sbin/resize2fs", "/dev/block/mmcblk0p9");');
+	info.script.AppendExtra('run_program("/tmp/install/bin/resize2fs_static", "/dev/block/mmcblk0p9");');
 	# run e2fsck
 	info.script.AppendExtra('run_program("/sbin/e2fsck", "-fy", "/dev/block/mmcblk0p9");');
